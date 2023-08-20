91136e1346bf5edece50ca1061e4adc9074fdf9e
Tristverm <emmanuelkyalo72@gmail.com>
2023-08-20 22:31:21 +0300

    Added header jsx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import ThemeProvider from "./contexts/ThemeProvider";
 import Header from "./components/Header";
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,28 @@
 import { useContext } from "react";
 import React from "react";
-function Header() {}
+import { themeContext } from "../contexts/ThemeProvider";
+function Header() {
+
+
+
+  return (
+    <header
+      className={`p-4 ${
+        darkMode ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-800"
+      }`}
+    >
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-semibold">Theme Toggle App</h1>
+        <button
+          onClick={toggleTheme}
+          className={`p-2 rounded ${
+            darkMode ? "bg-gray-800 text-white" : "bg-white text-gray-800"
+          }`}
+        >
+          Toggle Theme
+        </button>
+      </div>
+    </header>
+  );
+}
 export default Header;
