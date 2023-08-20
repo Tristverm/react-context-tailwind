695055ae74fe90a862df4ad1342e21c4b66a51ff
Tristverm <emmanuelkyalo72@gmail.com>
2023-08-20 22:47:43 +0300

    Added react context API derived switchTheme handler succesfully

---
+++ src/components/Hero.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { themeContext } from "../contexts/ThemeProvider";
+
+function Hero() {
+  return (
+    <themeContext.Consumer>
+      {({ darkMode}) => {
+        return(
+          <div
+            className={`container mx-auto p-4 mt-4 ${
+              darkMode ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-800"
+            }`}
+          >
+            <p>
+              This is the content area. The theme is currently{" "}
+              {darkMode ? "dark" : "light"}.
+            </p>
+          </div>
+        );
+      }}
+    </themeContext.Consumer>
+  );
+}
+
+export default Hero;
