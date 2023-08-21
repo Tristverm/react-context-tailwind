853ca5a26cf7db1a5d0d4a6327b96edef6047ec5
Tristverm <emmanuelkyalo72@gmail.com>
2023-08-21 21:50:58 +0300

    Added form compoent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import ThemeProvider from "./contexts/ThemeProvider";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <ThemeProvider>
       <Header />
+      <Hero />
     </ThemeProvider>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,15 +4,15 @@ import { themeContext } from "../contexts/ThemeProvider";
 function Hero() {
   return (
     <themeContext.Consumer>
-      {({ darkMode}) => {
-        return(
+      {({ darkMode }) => {
+        return (
           <div
-            className={`container mx-auto p-4 mt-4 ${
+            className={`container text-center mx-auto p-4 mt-4 ${
               darkMode ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-800"
             }`}
           >
             <p>
-              This is the content area. The theme is currently{" "}
+              This is the content area. The theme is currently
               {darkMode ? "dark" : "light"}.
             </p>
           </div>
