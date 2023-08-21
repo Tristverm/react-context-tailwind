4afbc3e79ade2ec49395f3aa34fc65d7a556fdff
Tristverm <emmanuelkyalo72@gmail.com>
2023-08-21 22:48:20 +0300

    Starter foprm

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import ThemeProvider from "./contexts/ThemeProvider";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Form from "./components/Form";
 
 function App() {
   return (
     <ThemeProvider>
       <Header />
       <Hero />
+      <Form />
     </ThemeProvider>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Form() {
   return (
-    <div>Form</div>
-  )
+    <div className="container mx-auto mt-4 flex flex-row justify-center items-center">
+      <form className="bg-gray-100 flex-1 flex flex-col justify-center ">
+        <legend className="text-bold text-xl text-center ">
+          Enter Details
+        </legend>
+        <div className="flex flex-row space-x-16 my-8 self-center">
+          <label>Surname</label>
+          <input />
+        </div>
+        <div className="flex flex-row space-x-16 my-8 self-center">
+          <label>Surname</label>
+          <input />
+        </div>
+        <div className="flex flex-row space-x-16 my-8 self-center">
+          <label>Surname</label>
+          <input />
+        </div>
+        <div className="flex flex-row space-x-16 my-8 self-center">
+          <label>Surname</label>
+          <input />
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Form
+export default Form;
